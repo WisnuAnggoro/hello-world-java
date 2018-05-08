37d2af29a8a08b10f29e30c4552bb7fdb22db835
Wisnu Anggoro <wisnu@anggoro.net>
2018-05-08 15:36:23 +0700

    Int Variable
    Try to use int data type

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotalNumber = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotalNumber);
     }
 }
